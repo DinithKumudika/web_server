8e6296c00194be4eb75afb93db6961ace4a36c5d
DinithKumudika <dinithwalpitagama@gmail.com>
2023-02-11 22:14:53 +0530

    constants defined

---
+++ src/constants.h
@@ -0,0 +1,5 @@
+#define SERVER_ROOT "../root"
+#define DEFAULT_PORT 80
+#define HOST_OS_WIN "windows"
+#define HOST_OS_LINUX "linux"
+#define _WIN32_WINNT 0x601
---
+++ src/server.h
@@ -0,0 +1,31 @@
+#include <sys/types.h>
+#include "constants.h"
+
+#ifdef _WIN32
+     #include <winsock.h>
+     #include <WS2tcpip.h>
+     #include <WS2spi.h>
+     #define HOST_OS HOST_OS_WIN
+#elif __linux__
+     #include <unistd.h>
+     #include <sys/socket.h>
+     #include <netinet/in.h>
+     #include <netinet/tcp.h>
+     #include <arpa/inet.h>
+     #define HOST_OS HOST_OS_LINUX
+#endif
+
+typedef struct Server
+{
+     int domain;
+     int protocol;
+     int service;
+     u_long net_interface;
+     int port;
+     int backlog;
+
+     struct sockaddr_in address;
+     int socket;
+} Server;
+
+struct Server server_init(int domain, int protocol, int service, u_long net_interface, int port, int backlog);
