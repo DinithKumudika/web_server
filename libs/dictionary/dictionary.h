ee5171d4d00f45518660bc4340218acd5509dbea
DinithKumudika <dinithwalpitagama@gmail.com>
2023-04-15 00:54:53 +0530

    implementing dictionary constructor and destructor

--- libs/dictionary/dictionary.h
+++ libs/dictionary/dictionary.h
@@ -11,9 +11,13 @@ Entry entry_init(void *key, int key_size, void *val, int val_size);
 
 void entry_del(Entry *entry);
 
-void insert(void *val);
+Dictionary dict_init();
+void dict_del(Dictionary *dict);
+
+void insert(void *key, int key_size, void *val, int val_size, Dictionary *dict);
+void search(void *key, Dictionary *dict);
 void display();
 void remove(void *key);
 void update();
-void search(void *key);
+
 
